cd98adf97eedddb640d9cfeaf46965bf5ecf6a7d
sudhanshupandey7294 <sudhanshupandey738@gmail.com>
2026-01-06 19:33:25 +0530

    fro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../sections/Hero'
 import Services from '../sections/Services'
 import HowItWorks from '../sections/HowItWorks'
-import Chatbot from '../sections/Chatbot';
+// import Chatbot from '../sections/Chatbot';
 // import Team from '../sections/Team'
 // import Testimonials from '../sections/Testimonials'
 import Contact from '../sections/Contact'
@@ -20,7 +20,7 @@ export default function Home(){
       {/* <Team /> */}
       {/* <Testimonials /> */}
       <Contact />
-      <Chatbot />
+      {/* <Chatbot /> */}
     </div>
   )
 }
